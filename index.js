06c83fd8e962f222e760f5e7dbd3106c6fa8d1af
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2023-04-18 12:07:28 +0600

    secure user authorization with Json Web Token

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
 const app = express();
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+async function run(){
+    try{
+        //JWT
+        
+        app.post('/jwt', async (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_JWT_TOKEN, {expiresIn: '1d'});
+            res.send({token});
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
 
 app.get('/', (req, res)=>{
     res.send('SPX Rocket Server Running..')
